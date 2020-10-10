33d51956f6839c278a133c831fce0da12288507f
Artsiom Biba <biba.artsiom@gmail.com>
2020-10-10 15:54:28 -0400

    deployment updates

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -6,62 +6,56 @@ import Typography from '@material-ui/core/Typography';
 
 import Context from '../../context';
 import { ME_QUERY } from '../../graphql/queries';
-import { BASE_URL } from '../../client'
+import { BASE_URL } from '../../client';
 
 const Login = ({ classes }) => {
-  const { dispatch } = useContext(Context)
+	const { dispatch } = useContext(Context);
 
-  const onSuccess = async googleUser => {
-    try {
-      const idToken = googleUser.getAuthResponse().id_token;
-      const client = new GraphQLClient(BASE_URL, {
-        headers: { authorization: idToken }
-      });
-      const { me } = await client.request(ME_QUERY);
-      dispatch({ type: "LOGIN_USER", payload: me });
-      dispatch({ type: "IS_LOGGED_IN", payload: googleUser.isSignedIn() });
-    } catch (err) {
-      onFailure(err);
-    }
+	const onSuccess = async (googleUser) => {
+		try {
+			const idToken = googleUser.getAuthResponse().id_token;
+			const client = new GraphQLClient(BASE_URL, {
+				headers : { authorization: idToken },
+				mode    : 'cors'
+			});
+			const { me } = await client.request(ME_QUERY);
+			dispatch({ type: 'LOGIN_USER', payload: me });
+			dispatch({ type: 'IS_LOGGED_IN', payload: googleUser.isSignedIn() });
+		} catch (err) {
+			onFailure(err);
+		}
+	};
 
-  };
+	const onFailure = (err) => {
+		console.error('Error logging in', err);
+		dispatch({ type: 'IS_LOGGED_IN', payload: false });
+	};
 
-  const onFailure = err => {
-    console.error('Error logging in', err);
-    dispatch({ type: "IS_LOGGED_IN", payload: false });
-  }
-
-  return (
-    <div className={classes.root}>
-      <Typography
-        component="h1"
-        variant="h3"
-        gutterBottom
-        noWrap
-        style={{ color: "rgb(66, 133, 244" }}
-      >
-        Welcome!
-      </Typography>
-      <GoogleLogin
-        clientId="1038659111441-0qsqon64o0sqhejl2die1c9jm9vvvaab.apps.googleusercontent.com"
-        onSuccess={onSuccess}
-        onFailure={onFailure}
-        isSignedIn={true}
-        theme="dark"
-        buttonText="Login with Google"
-      />
-    </div>
-  )
+	return (
+		<div className={classes.root}>
+			<Typography component="h1" variant="h3" gutterBottom noWrap style={{ color: 'rgb(66, 133, 244' }}>
+				Welcome!
+			</Typography>
+			<GoogleLogin
+				clientId="1038659111441-0qsqon64o0sqhejl2die1c9jm9vvvaab.apps.googleusercontent.com"
+				onSuccess={onSuccess}
+				onFailure={onFailure}
+				isSignedIn={true}
+				theme="dark"
+				buttonText="Login with Google"
+			/>
+		</div>
+	);
 };
 
 const styles = {
-  root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
-  }
+	root : {
+		height         : '100vh',
+		display        : 'flex',
+		justifyContent : 'center',
+		flexDirection  : 'column',
+		alignItems     : 'center'
+	}
 };
 
 export default withStyles(styles)(Login);
