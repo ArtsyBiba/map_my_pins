2839beba5c67cf77d8afbefd0c81e2363a14bcdd
Artsiom Biba <biba.artsiom@gmail.com>
2020-10-10 12:03:24 -0400

    setting up subscriptions

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -14,159 +14,145 @@ import { useClient } from '../../client';
 import { CREATE_PIN_MUTATION } from '../../graphql/mutations';
 
 const CreatePin = ({ classes }) => {
-  const { state, dispatch } = useContext(Context);
-  const client = useClient();
-  const [title, setTitle] = useState("");
-  const [image, setImage] = useState("");
-  const [content, setContent] = useState("");
-  const [submitting, setSubmitting] = useState(false);
+	const { state, dispatch } = useContext(Context);
+	const client = useClient();
+	const [ title, setTitle ] = useState('');
+	const [ image, setImage ] = useState('');
+	const [ content, setContent ] = useState('');
+	const [ submitting, setSubmitting ] = useState(false);
 
-  const handleDeleteDraft = () => {
-    setTitle("")
-    setImage("")
-    setContent("")
-    dispatch({ type: "DELETE_DRAFT" })
-  };
+	const handleDeleteDraft = () => {
+		setTitle('');
+		setImage('');
+		setContent('');
+		dispatch({ type: 'DELETE_DRAFT' });
+	};
 
-  const handleImageUpload = async () => {
-    const data = new FormData();
-    data.append("file", image);
-    data.append("upload_preset", "map_my_pins");
-    data.append("cloud_name", "bjj-hub");
-    const res = await axios.post(
-      "https://api.cloudinary.com/v1_1/bjj-hub/image/upload",
-      data
-    );
-    return res.data.url
-  };
+	const handleImageUpload = async () => {
+		const data = new FormData();
+		data.append('file', image);
+		data.append('upload_preset', 'map_my_pins');
+		data.append('cloud_name', 'bjj-hub');
+		const res = await axios.post('https://api.cloudinary.com/v1_1/bjj-hub/image/upload', data);
+		return res.data.url;
+	};
 
-  const handleSubmit = async event => {
-    try {
-      event.preventDefault();
-      setSubmitting(true);
-      const url = await handleImageUpload();
-      const { latitude, longitude } = state.draft;
-      const variables = { title, image: url, content, latitude, longitude };
-      const { createPin } = await client.request(CREATE_PIN_MUTATION, variables);
-      dispatch({ type: "CREATE_PIN", payload: createPin });
-      handleDeleteDraft();
-    } catch (err) {
-      setSubmitting(false);
-      console.error("Error creating pin", err);
-    }
-  };
+	const handleSubmit = async (event) => {
+		try {
+			event.preventDefault();
+			setSubmitting(true);
+			const url = await handleImageUpload();
+			const { latitude, longitude } = state.draft;
+			const variables = { title, image: url, content, latitude, longitude };
+			await client.request(CREATE_PIN_MUTATION, variables);
+			handleDeleteDraft();
+		} catch (err) {
+			setSubmitting(false);
+			console.error('Error creating pin', err);
+		}
+	};
 
-  return (
-    <form className={classes.form}>
-      <Typography
-        className={classes.alignCenter}
-        component="h2"
-        variant="h4"
-        color="secondary"
-      >
-        <LandscapeIcon className={classes.iconLarge} /> Pin Location
-      </Typography>
-      <div>
-        <TextField
-          name="title"
-          label="Title"
-          placeholder="Insert pin name"
-          onChange={e => setTitle(e.target.value)}
-        />
-        <input
-          accept="image/*"
-          id="image"
-          type="file"
-          className={classes.input}
-          onChange={e => setImage(e.target.files[0])}
-        />
-        <label htmlFor="image">
-          <Button
-            component="span"
-            size="small"
-            style={{ color: image && "green" }}
-            className={classes.button}
-          >
-            <AddAPhotoIcon />
-          </Button>
-        </label>
-      </div>
-      <div className={classes.contentField}>
-        <TextField
-          name="content"
-          label="Content"
-          multiline
-          rows="6"
-          margin="normal"
-          fullWidth
-          variant="outlined"
-          onChange={e => setContent(e.target.value)}
-        />
-      </div>
-      <div>
-        <Button
-          className={classes.button}
-          variant="contained"
-          color="primary"
-          onClick={handleDeleteDraft}
-        >
-          <ClearIcon className={classes.leftIcon} />
-          Discard
-        </Button>
-        <Button
-          type="submit"
-          className={classes.button}
-          variant="contained"
-          color="secondary"
-          disabled={!title.trim() || !content.trim || !image || submitting}
-          onClick={handleSubmit}
-        >
-          Submit
-          <SaveIcon className={classes.rightIcon} />
-        </Button>
-      </div>
-    </form>
-  );
+	return (
+		<form className={classes.form}>
+			<Typography className={classes.alignCenter} component="h2" variant="h4" color="secondary">
+				<LandscapeIcon className={classes.iconLarge} /> Pin Location
+			</Typography>
+			<div>
+				<TextField
+					name="title"
+					label="Title"
+					placeholder="Insert pin name"
+					onChange={(e) => setTitle(e.target.value)}
+				/>
+				<input
+					accept="image/*"
+					id="image"
+					type="file"
+					className={classes.input}
+					onChange={(e) => setImage(e.target.files[0])}
+				/>
+				<label htmlFor="image">
+					<Button
+						component="span"
+						size="small"
+						style={{ color: image && 'green' }}
+						className={classes.button}
+					>
+						<AddAPhotoIcon />
+					</Button>
+				</label>
+			</div>
+			<div className={classes.contentField}>
+				<TextField
+					name="content"
+					label="Content"
+					multiline
+					rows="6"
+					margin="normal"
+					fullWidth
+					variant="outlined"
+					onChange={(e) => setContent(e.target.value)}
+				/>
+			</div>
+			<div>
+				<Button className={classes.button} variant="contained" color="primary" onClick={handleDeleteDraft}>
+					<ClearIcon className={classes.leftIcon} />
+					Discard
+				</Button>
+				<Button
+					type="submit"
+					className={classes.button}
+					variant="contained"
+					color="secondary"
+					disabled={!title.trim() || !content.trim || !image || submitting}
+					onClick={handleSubmit}
+				>
+					Submit
+					<SaveIcon className={classes.rightIcon} />
+				</Button>
+			</div>
+		</form>
+	);
 };
 
-const styles = theme => ({
-  form: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    paddingBottom: theme.spacing.unit
-  },
-  contentField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-    width: "95%"
-  },
-  input: {
-    display: "none"
-  },
-  alignCenter: {
-    display: "flex",
-    alignItems: "center"
-  },
-  iconLarge: {
-    fontSize: 40,
-    marginRight: theme.spacing.unit
-  },
-  leftIcon: {
-    fontSize: 20,
-    marginRight: theme.spacing.unit
-  },
-  rightIcon: {
-    fontSize: 20,
-    marginLeft: theme.spacing.unit
-  },
-  button: {
-    marginTop: theme.spacing.unit * 2,
-    marginBottom: theme.spacing.unit * 2,
-    marginRight: theme.spacing.unit,
-    marginLeft: 0
-  }
+const styles = (theme) => ({
+	form         : {
+		display        : 'flex',
+		justifyContent : 'center',
+		alignItems     : 'center',
+		flexDirection  : 'column',
+		paddingBottom  : theme.spacing.unit
+	},
+	contentField : {
+		marginLeft  : theme.spacing.unit,
+		marginRight : theme.spacing.unit,
+		width       : '95%'
+	},
+	input        : {
+		display : 'none'
+	},
+	alignCenter  : {
+		display    : 'flex',
+		alignItems : 'center'
+	},
+	iconLarge    : {
+		fontSize    : 40,
+		marginRight : theme.spacing.unit
+	},
+	leftIcon     : {
+		fontSize    : 20,
+		marginRight : theme.spacing.unit
+	},
+	rightIcon    : {
+		fontSize   : 20,
+		marginLeft : theme.spacing.unit
+	},
+	button       : {
+		marginTop    : theme.spacing.unit * 2,
+		marginBottom : theme.spacing.unit * 2,
+		marginRight  : theme.spacing.unit,
+		marginLeft   : 0
+	}
 });
 
 export default withStyles(styles)(CreatePin);
